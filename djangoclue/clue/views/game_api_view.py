e87180fa2250b14c537b455f60127be540f2c53e
OpenShift guest <pd_hummel@yahoo.com>
2014-01-29 14:44:48 -0500

    Rest api for games.

---
+++ djangoclue/clue/views/game_api_view.py
@@ -0,0 +1,21 @@
+from clue.models import Game
+from clue.serializers import GameSerializer, GameSecretSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
+
+
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+class GameSecretDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSecretSerializer
